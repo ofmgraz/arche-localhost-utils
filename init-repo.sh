9e8ff39a3adc584ac50f4474deb8f3332ee537f4
Mateusz Żółtak <zozlak@zozlak.org>
2021-03-10 19:22:42 +0100

    Note on initialization added to the README

--- init-repo.sh
+++ init-repo.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
-VOLUMES_DIR=~/repos/arche/repo
+#!/bin/bash
+cd "`dirname $0`"
+source volumesdir.sh
 git clone https://github.com/acdh-oeaw/arche-docker-config.git -b arche $VOLUMES_DIR/config
-cp users.yaml $VOLUMES_DIR/config/initScripts/users.yaml
-#cp custom_config/dissServices.ttl $VOLUMES_DIR/config/initScripts/dissServices.ttl
+cp users.yaml "$VOLUMES_DIR/config/initScripts/users.yaml"
+cp custom_config/dissServices.ttl "$VOLUMES_DIR/config/initScripts/dissServices.ttl"
 for i in data tmp postgresql log vendor gui; do
-    mkdir -p $VOLUMES_DIR/$i
+    mkdir -p "$VOLUMES_DIR/$i"
 done
 
--- reset-repo.sh
+++ reset-repo.sh
@@ -1,10 +1,14 @@
-#!/bin/sh
-VOLUMES_DIR=~/repos/arche/repo
-rm -rf $VOLUMES_DIR/config
-git clone https://github.com/acdh-oeaw/arche-docker-config.git -b arche $VOLUMES_DIR/config
-cp users.yaml ./repo/config/initScripts/users.yaml
-cp custom_config/dissServices.ttl ./repo/config/initScripts/dissServices.ttl
+#!/bin/bash
+cd "`dirname $0`"
+source volumesdir.sh
+#
+docker stop acdh-repo
 for i in data tmp postgresql log vendor gui; do
-    rm -rf $VOLUMES_DIR/$i
-    mkdir -p $VOLUMES_DIR/$i
+    rm -rf "$VOLUMES_DIR/$i"
+    mkdir "$VOLUMES_DIR/$i"
 done
+rm -fr "$VOLUMES_DIR/config"
+git clone https://github.com/acdh-oeaw/arche-docker-config.git -b arche "$VOLUMES_DIR/config"
+cp users.yaml "$VOLUMES_DIR/config/initScripts/users.yaml"
+cp custom_config/dissServices.ttl "$VOLUMES_DIR/config/initScripts/dissServices.ttl"
+
