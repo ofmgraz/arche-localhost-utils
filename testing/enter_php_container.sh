3b4b529ce3752744ff59e3d90b79a1206efae28a
Mateusz Żółtak <zozlak@zozlak.org>
2024-03-08 13:44:31 +0100

    testing/enter_php_container.sh: set the working directory to /data

--- testing/enter_php_container.sh
+++ testing/enter_php_container.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-docker run --rm -ti -u $UID --entrypoint bash --network host -v ${PWD}:/data acdhch/arche-filechecker
+docker run --rm -ti -u $UID --entrypoint bash --network host -v ${PWD}:/data -w /data acdhch/arche-filechecker
